5764e7ad08e7ddd13439be19f61f5834cd63f7f0
preetnagda <i_preet.n@olcademy.in>
2019-09-27 12:20:08 +0530

    working

--- IICproject/IICapp/views.py
+++ IICproject/IICapp/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from IICapp import models
+import os
 
 # Create your views here.
 
 def login(request):
+    name = ""
     if (request.POST):
-        return HttpResponse("HelloGET")
-    else:
-        return HttpResponse("hello")
+        # print(request["logid"])
+        username = request.POST["logid"]
+        password = request.POST["psw"]
+        loggedincontext = {
+            "students" : models.Student.objects.raw('SELECT * from IICapp_student')
+        }
+        for data in models.Teacher.objects.raw('SELECT * from IICapp_teacher'):
+            if(data.username==username):
+                if(data.password==password):
+                    request.session["username"] = username;
+                    return render(request,"home.html",loggedincontext)
+
+    context = {
+    "name":name
+    }
+    return render(request,"login.html",context)
