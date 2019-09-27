93fb1fa1fc34569cd11226b258d34298f537433d
preetnagda <i_preet.n@olcademy.in>
2019-09-27 11:05:07 +0530

    database

--- IICproject/IICapp/models.py
+++ IICproject/IICapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    roll_no = models.IntegerField()
+    name = models.CharField(max_length=50)
+    std = models.IntegerField()
+    stud_class = models.IntegerField()
+    parent_phone_number = models.CharField(max_length=10)
+    parent_email_id = models.EmailField()
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    std = models.IntegerField()
+    teacher_class = models.IntegerField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+class Attendance(models.Model):
+     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+     attendace = models.BooleanField()
+     date = models.DateField()
